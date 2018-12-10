7e30ea2ec1c4ee1d45dce6e71cb7399483ab7da5
Peter <peterhsieh@edallianz.com>
2018-12-10 15:09:37 +0800

    Basic migration ok

---
+++ database/migrations/2018_12_07_101641_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
--- readme.md
+++ readme.md
@@ -56,3 +56,10 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+# Develop note
+
+## Main reference
+https://blog.pusher.com/build-rest-api-laravel-api-resources/
+### Official reference
+https://laravel.com/docs/5.5/
