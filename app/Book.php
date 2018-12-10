3407aa81d6d46e77ec1b9214a9db5291372beab8
Peter <peterhsieh@edallianz.com>
2018-12-10 17:56:58 +0800

    Init model relation

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    // fields
+    protected $fillable = [
+        'user_id', 'title', 'description'
+    ];
+
+    // book user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // book rating
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
--- app/Rating.php
+++ app/Rating.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    // fields
+    protected $fillable = [
+        'book_id', 'user_id', 'rating'
+    ];
+
+    // rating book
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
